d8f51f0476f2bd6dc69354169db5c36d6d58111b
brian <nsklobuchar666@gmail.com>
2020-09-27 14:31:29 -0400

    FINALIZACION DE ANIMACION

--- Animacion.js
+++ Animacion.js
@@ -124,3 +124,64 @@ function mostrarscroll6()
 
 
 window.addEventListener('scroll',mostrarscroll6);
+
+let animado7= document.querySelectorAll(".animacion7");
+function mostrarscroll7()
+{
+    let scrolltop3 = document.documentElement.scrollTop;
+    //contador de las clases animado
+    for(var i =0;i<animado7.length;i++)
+    {
+        let alturaAnimado3 = animado7[i].offsetTop;
+        if(alturaAnimado3 +2750 <  scrolltop3)
+        {
+            animado7[i].style.opacity=1;
+            animado7[i].classList.add("animacionMostraIzquierda");
+            
+        }
+    }
+}
+
+
+window.addEventListener('scroll',mostrarscroll7);
+
+
+let animado8= document.querySelectorAll(".animacion8");
+function mostrarscroll8()
+{
+    let scrolltop3 = document.documentElement.scrollTop;
+    //contador de las clases animado
+    for(var i =0;i<animado8.length;i++)
+    {
+        let alturaAnimado3 = animado8[i].offsetTop;
+        if(alturaAnimado3 +3250 <  scrolltop3)
+        {
+            animado8[i].style.opacity=1;
+            animado8[i].classList.add("animacionMostraDerecha");
+            
+        }
+    }
+}
+
+
+window.addEventListener('scroll',mostrarscroll8);
+
+let animado9= document.querySelectorAll(".animacion9");
+function mostrarscroll9()
+{
+    let scrolltop3 = document.documentElement.scrollTop;
+    //contador de las clases animado
+    for(var i =0;i<animado9.length;i++)
+    {
+        let alturaAnimado3 = animado9[i].offsetTop;
+        if(alturaAnimado3 +3750 <  scrolltop3)
+        {
+            animado9[i].style.opacity=1;
+            animado9[i].classList.add("animacionMostraIzquierda");
+            
+        }
+    }
+}
+
+
+window.addEventListener('scroll',mostrarscroll9);

968459e1a757351a2ac9c0a49d66b048ca868851
brian <nsklobuchar666@gmail.com>
2020-09-26 22:38:56 -0400

    animacion

--- Animacion.js
+++ Animacion.js
@@ -7,9 +7,10 @@ function mostrarscroll()
     for(var i =0;i<animado.length;i++)
     {
         let alturaAnimado = animado[i].offsetTop;
-        if(alturaAnimado + 700 <  scrolltop)
+        if(alturaAnimado + 600 <  scrolltop)
         {
             animado[i].style.opacity=1;
+            animado[i].classList.add("animacionMostraArriba");
             
         }
     }
